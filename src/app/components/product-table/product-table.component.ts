99bf06fbab59cbaab2ca8cf49d7804dd89e2a301
LascanoAldahir <aldahirlascano@gmail.com>
2025-05-08 17:47:39 -0500

    explicaciones

--- src/app/components/product-table/product-table.component.ts
+++ src/app/components/product-table/product-table.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../api/api.service';
 
+////Define como se va a llamar al componente en HTML
 @Component({
   selector: 'app-product-table',
   standalone: true,
@@ -12,30 +13,41 @@ import { ApiService } from '../../api/api.service';
   templateUrl: './product-table.component.html',
   styleUrl: './product-table.component.css'
 })
+//PROPIEDADES DEL COMPONENTE
 export class ProductTableComponent implements OnInit {
+  //Servicios inyectados para navegación y comunicación con API
   private router = inject(Router);
   private productApi = inject(ApiService);
 
+  //Almacena el texto que el usuario escribe en el buscador
   searchText: string = '';
+  
+  //Numero de productos por pagina
   pageSize: number = 5;
-
+//Array con todos los productos
   products: Product[] = [];
   productosFiltrados: Product[] = [];
 
   productoAEliminar: Product | null = null;
   mostrarModal: boolean = false;
 
+  //Lama al metodo para cargar productos desde la API 
   ngOnInit(): void {
     this.cargarProductos();
   }
 
+//Llama al método getAll() del servicio ApiServer
   cargarProductos() {
+    //Se suscribe al observable para recibir los datos
     this.productApi.getAll().subscribe((data) => {
+      //almacena los productos en la propiedad producs
       this.products = data;
+      //Llama a este metodo para mostrar los productos
       this.actualizarFiltrados();
     });
   }
 
+//Filtra los productos basandose
   actualizarFiltrados() {
     this.productosFiltrados = this.products
       .filter(p =>
@@ -56,7 +68,6 @@ export class ProductTableComponent implements OnInit {
   irAAgregar() {
     this.router.navigate(['/agregar']);
   }
-
 
   confirmarEliminacion(product: Product) {
     this.productoAEliminar = product;
